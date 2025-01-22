84521a16da6ae76cdf3842f3b6e0078a9e936f69
cantbreath123 <rocanorhett735@gmail.com>
2025-01-22 23:54:58 +0800

    fix(api): asyncio not found

--- api/index.py
+++ api/index.py
@@ -11,6 +11,7 @@ Note:
 To use arbitrary callback data, you must install PTB via
 `pip install "python-telegram-bot[callback-data]"`
 """
+import asyncio
 import logging
 from datetime import datetime, timezone, timedelta
 from telegram import Update, Bot
